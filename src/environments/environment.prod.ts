7f230a4962244146ef0181d0b92cccf74c760113
Brock Anderson <brock@bandersgeo.ca>
2018-07-03 16:30:28 -0700

    updated environment with URL for argg-api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   bcdc_base_url: "https://catalogue.data.gov.bc.ca",
   bcdc_api_path: "/api/3/action",
-  argg_api_base_url: "http://arg-api.url.here",
+  argg_api_base_url: "http://argg-api.pathfinder.bcgov",
   license_list_url: "https://licenses-bcdc.data.gov.bc.ca/bcdc_licenses.json"
 };
